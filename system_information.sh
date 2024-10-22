be28fd0f639df1b6a60fe3133667a923df795f9c
Hatimloha <139457449+Hatimloha@users.noreply.github.com>
2024-10-22 23:45:30 +0530

    Update system_information.sh

--- system_information.sh
+++ system_information.sh
@@ -1,10 +1,9 @@
 # This script will help to print the system infomration like memory, disk, & CPU: 
 #!/bin/bash
 
-set -x # debug mode
+set -x # Enable debug mode to show each command before executi
 
-df -h
-
-free -g
-
-nproc
+df -h   # Displays disk space usage in human-readable format
+free -g # Displays memory usage in gigabytes
+nproc   # Shows the number of available processing units (CPU cores)
+ps -ef  # Lists all running processes in the system
