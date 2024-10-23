3dde9b9042f8342a76322a24ac4509c4d0ef38c1
Hatimloha <139457449+Hatimloha@users.noreply.github.com>
2024-10-23 08:48:19 +0530

    Create Log_Rotation_and_Compression_Script.sh

---
+++ Log_Rotation_and_Compression_Script.sh
@@ -0,0 +1,11 @@
+# A simple log rotation script that compresses logs older than 7 days.
+
+#!/bin/bash
+
+# Log directory
+LOG_DIR="/var/log/myapp"
+
+# Find logs older than 7 days and compress them
+find "$LOG_DIR" -type f -name "*.log" -mtime +7 -exec gzip {} 
+
+echo "Log files older than 7 days have been compressed."\;
