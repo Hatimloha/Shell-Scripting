833f722a4d67b6c329f56885fc0c82ef7681268f
Hatimloha <139457449+Hatimloha@users.noreply.github.com>
2024-10-25 17:11:41 +0530

    Update System_Uptime_Check.sh

--- System_Uptime_Check.sh
+++ System_Uptime_Check.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+HOURS_THRESHOLD=24
+UPTIME_HOURS=$(awk '{prin int($1/3600)}' /proc/uptime)
+
+echo "system uptime: $UPTIME_THRESHOLD hours"
+if ["$UPTIME_HOURS" -gt "$HOURS_THRESHOLD"]; then
+  echo "system has been running for more then $HOURS_THRESHOLD hours."
+else 
+  echo "system uptime is within the limites."
+fi
