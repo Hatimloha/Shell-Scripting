9e8eae500e3a1956cf7de27806afe0d3fe36067b
Hatimloha <139457449+Hatimloha@users.noreply.github.com>
2024-11-01 21:40:20 +0530

    Update expression.sh

--- expression.sh
+++ expression.sh
@@ -5,9 +5,9 @@
 # Not divisible by 15
 ######################### 
 
-for i in {1, 100}; do
-if([`expr $1 % 3` == 0] || [`expr $1 % 5` == 0] || [`expr $1 % 15` != 0])
-then
-  echo"$1"
-fi;
+for i in {1..100}; do
+    if ( [ $((i % 3)) == 0 ] || [ $((i % 5)) == 0 ] || [ $((i % 15)) != 0 ] ); then 
+        echo "$i"
+    fi
 done
+
